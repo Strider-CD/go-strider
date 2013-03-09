6c581b4de25f093789cc1674fb729d600d628141
Peter Braden <peterbraden@peterbraden.co.uk>
2013-03-08 16:13:32 -0800

    Initial commit

---
+++ worker.js
@@ -0,0 +1,15 @@
+// Setup Worker
+module.exports = function(ctx, cb) {
+
+  ctx.addDetectionRule({
+      filename: "*.go"
+    , exists: true
+    , language: "go"
+    , framework: "go"
+    , prepare: "go get && go install"
+    , test: "go test"
+  })
+
+  console.log("go-strider extension loaded")
+  cb(null, null);
+}
